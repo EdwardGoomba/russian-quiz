09de4ab821052e5089b39edafe09d8a3e2d23735
EdwardGoomba <ask@edwardvdesign.com>
2017-12-01 20:51:30 -0500

    Display number of correct answers

--- js/quiz.js
+++ js/quiz.js
@@ -14,12 +14,15 @@ function print(message) {
   document.write(message);
 }
 
-for (var i = 0; i < questions.length; i ++) {
+for (var i = 0; i < questions.length; i += 1) {
   question = questions[i][0];
   answer = questions[i][1];
   response = parseInt(prompt(question));
 
   if (response === answer) {
-    correctAnswers++;
+    correctAnswers += 1;
   }
 }
+
+html = "You got " + correctAnswers + " question(s) right.";
+print(html);

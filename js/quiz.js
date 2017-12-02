2ccb9d241956f83c441747221e01afa01dab098b
EdwardGoomba <ask@edwardvdesign.com>
2017-12-01 20:35:21 -0500

    Initialize variables

--- js/quiz.js
+++ js/quiz.js
@@ -4,7 +4,11 @@ var questions = [
   ['What is the Russians favorite animal?'], ['Bear']
 ];
 
-
+var correctAnswers = 0;
+var question;
+var answer;
+var response;
+var html;
 
 function print(message) {
   document.write(message);

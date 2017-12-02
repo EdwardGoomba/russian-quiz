07d6c0673f51161e42e8baa1180d595791a30ad2
EdwardGoomba <ask@edwardvdesign.com>
2017-12-01 20:34:31 -0500

    Added questions for quiz

--- js/quiz.js
+++ js/quiz.js
@@ -1,3 +1,10 @@
+var questions = [
+  ['What do Russians love to drink'], ['Vodka'],
+  ['What are Russians favorite vegetable?'], ['Potatoes'],
+  ['What is the Russians favorite animal?'], ['Bear']
+];
+
+
 
 function print(message) {
   document.write(message);

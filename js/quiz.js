24a9ae476a02031030e80b65d6f035da29335b50
EdwardGoomba <ask@edwardvdesign.com>
2018-01-05 11:02:07 -0500

    Setup core quiz

--- js/quiz.js
+++ js/quiz.js
@@ -1,60 +1,20 @@
-var questions = [
-  {
-    question: 'What do Russians love to drink',
-    answer: 'Vodka'
-  },
-  {
-    question: 'What are Russians favorite vegetable?',
-    answer: 'Potatoes'
-  },
-  {
-    question: 'What is the Russians favorite animal?',
-    answer: 'Bear'
-  }
-];
-
-var correctAnswers = 0;
-var question;
-var answer;
-var response;
-var html;
-var correct = [];
-var incorrect = [];
-
-// Use to print message to page
-function print(message) {
-  var outputDiv = document.getElementById('output');
-  outputDiv.innerHTML = message;
+function Quiz(questions) {
+  this.score = 0;
+  this.questions = questions;
+  this.currentQuestionIndex = 0;
 }
 
-// Parse through array and add items to an ordered list
-function buildList(arr) {
-  var listHTML = '<ol>';
-  for (var i = 0; i < arr.length; i++) {
-    listHTML += '<li>' + arr[i] + '</li>';
+Quiz.prototype.guess = function(answer) {
+  if(this.getCurrentQuestion().isCorrectAnswer(answer)) {
+    this.score++;
   }
-  listHTML += '</ol>';
-  return listHTML;
-}
+  this.currentQuestionIndex++;
+};
 
-// Loop through array, access object and return responses
-for (var i = 0; i < questions.length; i += 1) {
-  question = questions[i].question;
-  answer = questions[i].answer;
-  response = prompt(question);
-  response = response;
-  if (response === answer) {
-    correctAnswers += 1;
-    correct.push(question);
-  } else {
-    incorrect.push(question);
-  }
-}
+Quiz.prototype.getCurrentQuestion = function() {
+  return this.questions[this.currentQuestionIndex];
+};
 
-//Concats answers and displays correct and incorrect answers as an ordered list
-html = "You got " + correctAnswers + " question(s) right."
-html += '<h2>You got these questions correct:</h2>';
-html += buildList(correct);
-html += '<h2>You got these questions wrong:</h2>';
-html += buildList(incorrect);
-print(html);
+Quiz.prototype.hasEnded = function() {
+  return this.currentQuestionIndex >= this.questions.length;
+}
